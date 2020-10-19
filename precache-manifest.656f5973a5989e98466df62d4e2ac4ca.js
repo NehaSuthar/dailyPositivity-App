e399d96d1fa58cd7ebc8f96765ca23d05f21d30e
Neha <nehasuthr@gmail.com>
2020-10-18 22:02:17 -0400

    Updates

---
+++ precache-manifest.656f5973a5989e98466df62d4e2ac4ca.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14575447efc16e486d690d54b5cea497",
+    "url": "/dailyPositivity-App/index.html"
+  },
+  {
+    "revision": "4035296bde5f8f8ed1bf",
+    "url": "/dailyPositivity-App/static/css/main.91926453.chunk.css"
+  },
+  {
+    "revision": "12799c4d64c766eb7973",
+    "url": "/dailyPositivity-App/static/js/2.324b891b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dailyPositivity-App/static/js/2.324b891b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4035296bde5f8f8ed1bf",
+    "url": "/dailyPositivity-App/static/js/main.7e49b437.chunk.js"
+  },
+  {
+    "revision": "3f3ff894c2932fe62634",
+    "url": "/dailyPositivity-App/static/js/runtime-main.c7aac315.js"
+  },
+  {
+    "revision": "4e66105da92fb08636c1fb6373f4828f",
+    "url": "/dailyPositivity-App/static/media/category-beach.4e66105d.jpg"
+  },
+  {
+    "revision": "a1491f68b43fe7cddfe40d11eb27ef0e",
+    "url": "/dailyPositivity-App/static/media/category-desert.a1491f68.jpg"
+  },
+  {
+    "revision": "daa6dbd406adbf2698844f73a8e2c82f",
+    "url": "/dailyPositivity-App/static/media/category-forest.daa6dbd4.jpg"
+  },
+  {
+    "revision": "d8750fd5f0eaad1dd33ae7fc70b65aa7",
+    "url": "/dailyPositivity-App/static/media/category-sky.d8750fd5.jpg"
+  },
+  {
+    "revision": "9cf94e4dfb2acee3c3948e2d7c270b61",
+    "url": "/dailyPositivity-App/static/media/category-space.9cf94e4d.jpg"
+  },
+  {
+    "revision": "5b4222fdc1503cd33c68fe4d06e3b935",
+    "url": "/dailyPositivity-App/static/media/category-twilight.5b4222fd.jpg"
+  }
+]);
